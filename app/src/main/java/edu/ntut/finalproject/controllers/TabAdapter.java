495fd20663b0687449ae1889ea550bcb82ce53dc
Ho-Ming Li <ming2007119@gmail.com>
2021-06-02 00:05:53 +0800

    update

--- app/src/main/java/edu/ntut/finalproject/controllers/TabAdapter.java
+++ app/src/main/java/edu/ntut/finalproject/controllers/TabAdapter.java
@@ -1,11 +1,18 @@
 package edu.ntut.finalproject.controllers;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.Lifecycle;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
 import java.util.ArrayList;
 
@@ -13,9 +20,10 @@ public class TabAdapter extends FragmentStateAdapter {
 
     private ArrayList<Fragment> TabList = new ArrayList<>();
 
-    public TabAdapter(@NonNull FragmentManager  fragmentManager, @NonNull Lifecycle lifecycle) {
+    public TabAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
         super(fragmentManager, lifecycle);
     }
+
 
     public void addFragment(Fragment fragment) {
         TabList.add(fragment);
--- app/src/main/java/edu/ntut/finalproject/views/MainActivity.java
+++ app/src/main/java/edu/ntut/finalproject/views/MainActivity.java
@@ -4,18 +4,20 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager2.widget.ViewPager2;
 
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.WindowManager;
+import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
 import edu.ntut.finalproject.R;
-import edu.ntut.finalproject.controllers.AddBookToSell;
-import edu.ntut.finalproject.controllers.ProgressBarAnimations;
+import edu.ntut.finalproject.controllers.TabAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,29 +29,31 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.open_app_loading);
+        setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        progressBar = findViewById(R.id.progressBar);
 
-        progressAnimation();
-        progressBar.setMax(100);
-        progressBar.setScaleX(2f);
-        progressBar.setScaleY(2f);
 
-        //shared preferences
-        mPreferences = getSharedPreferences(filename, MODE_PRIVATE);
+
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//
+//        progressBar = findViewById(R.id.progressBar);
+//
+//        progressAnimation();
+//        progressBar.setMax(100);
+//        progressBar.setScaleX(2f);
+//        progressBar.setScaleY(2f);
+//
+//        //shared preferences
+//        mPreferences = getSharedPreferences(filename, MODE_PRIVATE);
 
 
         //condition connected or not
-        if (mPreferences.contains("id")) {
-            Intent intent = new Intent(MainActivity.this, AddBookToSell.class);
-            startActivity(intent);
-        }
+//        if (mPreferences.contains("id")) {
+//            Intent intent = new Intent(MainActivity.this, AddBookToSell.class);
+//            startActivity(intent);
+//        }
 
 
     }
@@ -63,12 +67,6 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
-    }
-
-    public void progressAnimation() {
-        ProgressBarAnimations anim = new ProgressBarAnimations(this, progressBar, textView, 0f, 100f);
-        anim.setDuration(3500);
-        progressBar.setAnimation(anim);
     }
 
 }
